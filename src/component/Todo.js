820eced293585d0efeca5452d0827d51be70fd13
Nikitasathawane <106878255+Nikitasathawane@users.noreply.github.com>
2023-09-18 15:49:41 +0530

    almost completed

---
+++ src/component/Todo.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function Todo() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>To-Do App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a task..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
